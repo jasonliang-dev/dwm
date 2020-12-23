c9d3299adc99a49574e847814383acfdf02fbe8f
Jason Liang <jasonliang512@gmail.com>
2020-12-22 19:02:49 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dwm
+
+No mainline patches.
---
+++ base16.h
@@ -0,0 +1,56 @@
+typedef struct {
+	char *base00;
+	char *base01;
+	char *base02;
+	char *base03;
+	char *base04;
+	char *base05;
+	char *base06;
+	char *base07;
+	char *base08;
+	char *base09;
+	char *base0A;
+	char *base0B;
+	char *base0C;
+	char *base0D;
+	char *base0E;
+	char *base0F;
+} Base16Colors;
+
+static const Base16Colors base16tomorrownight = {
+	.base00 = "#1d1f21",
+	.base01 = "#282a2e",
+	.base02 = "#373b41",
+	.base03 = "#969896",
+	.base04 = "#b4b7b4",
+	.base05 = "#c5c8c6",
+	.base06 = "#e0e0e0",
+	.base07 = "#ffffff",
+	.base08 = "#cc6666",
+	.base09 = "#de935f",
+	.base0A = "#f0c674",
+	.base0B = "#b5bd68",
+	.base0C = "#8abeb7",
+	.base0D = "#81a2be",
+	.base0E = "#b294bb",
+	.base0F = "#a3685a",
+};
+
+static const Base16Colors base16onedark = {
+	.base00 = "#282c34",
+	.base01 = "#353b45",
+	.base02 = "#3e4451",
+	.base03 = "#545862",
+	.base04 = "#565c64",
+	.base05 = "#abb2bf",
+	.base06 = "#b6bdca",
+	.base07 = "#c8ccd4",
+	.base08 = "#e06c75",
+	.base09 = "#d19a66",
+	.base0A = "#e5c07b",
+	.base0B = "#98c379",
+	.base0C = "#56b6c2",
+	.base0D = "#61afef",
+	.base0E = "#c678dd",
+	.base0F = "#be5046",
+};
